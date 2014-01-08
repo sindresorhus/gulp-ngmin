ad67fc1941b38129b25e53793ccdc72fcc5278dc
Sindre Sorhus <sindresorhus@gmail.com>
2014-01-08 23:37:51 +0100

    tweaks

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 'use strict';
-var es = require('event-stream');
-var ngmin = require('ngmin');
 var gutil = require('gulp-util');
+var through = require('through');
+var ngmin = require('ngmin');
 
 module.exports = function () {
-	return es.through(function (file) {
-		if (file.isNull()) return this.emit('data', file); // pass along
+	return through(function (file) {
+		if (file.isNull()) {
+			return this.queue(file);
+		}
+
+		if (file.isStream()) {
+			return this.emit('error', new gutil.PluginError('gulp-ngmin', 'Streaming not supported'));
+		}
 
 		try {
 			file.contents = new Buffer(ngmin.annotate(file.contents.toString()));
 		} catch (err) {
-            		this.emit('error', new gutil.PluginError('gulp-ngmin', err));
-        	}
-		this.emit('data', file);
+			this.emit('error', new gutil.PluginError('gulp-ngmin', err));
+		}
+
+		this.queue(file);
 	});
 };
